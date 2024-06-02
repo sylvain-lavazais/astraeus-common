fe411b8b183951efac28d242786b2697ee74e2fc
lavazais-sylvain <sylvain.lavazais@zenika.com>
2024-06-02 09:00:27 +0200

    refactor(code style): fix line wrapping issue

--- src/astraeus_common/error/system_not_found.py
+++ src/astraeus_common/error/system_not_found.py
@@ -1,11 +1,14 @@
 class SystemNotFound(Exception):
     """
-    The SystemNotFound class is an exception class that is raised when a specific system or a component of a system is not found.
+    The SystemNotFound class is an exception class raised when a specific
+    system or a component of a system is not found.
 
     It inherits directly from the base class `Exception` in Python.
 
     Usage:
-        When an application encounters a situation where a system or a component of a system is expected to be found, but it is not, the SystemNotFound exception can be raised.
+        When an application encounters a situation
+        where a system or a component of a system is expected to be found,
+        but it is not, the SystemNotFound exception can be raised.
 
     Example:
         >>> raise SystemNotFound("System not found")
